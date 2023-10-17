ff90b7e70d9e4fecadd2f35ff24cbbd4c41fcfd3
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-10-17 15:51:50 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using XUnitTDD;
+
+namespace CalculadoraTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Calculadora calculadora = new Calculadora();
+        }
+    }
+}
