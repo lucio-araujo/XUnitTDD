3561cb2f2b0baa1fae186a05909b9427d5bb855a
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-10-17 16:30:54 -0300

    Demais Operações

--- CalculadoraTest/UnitTest1.cs
+++ CalculadoraTest/UnitTest1.cs
@@ -14,5 +14,46 @@ namespace CalculadoraTest
 
             Assert.Equal(resultado, resultadoCalculadora);
         }
+
+        [Theory]
+        [InlineData(2, 1, 1)]
+        public void TestSubtrair(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.Subtrair(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(2, 3, 6)]
+        public void TestMultiplicar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.Multiplicar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 5)]
+        public void TestDividir(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+
+            int resultadoCalculadora = calc.Dividir(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Fact]
+        public void TestarDivisaoPorZero()
+        {
+            Calculadora calc = new Calculadora();
+
+            Assert.Throws<DivideByZeroException>(() => calc.Dividir(2, 0));
+        }
     }
 }
