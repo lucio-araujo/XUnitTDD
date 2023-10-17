5c246d92c5f80b6c6ec71e7c62afde882d24d312
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-10-17 17:13:35 -0300

    Desafio Concluído

--- XUnitTDD/Calculadora.cs
+++ XUnitTDD/Calculadora.cs
@@ -8,29 +8,50 @@ namespace XUnitTDD
 {
     public class Calculadora
     {
+
+        private List<string> listHistorico;
+
+        public Calculadora()
+        {
+            listHistorico = new List<string>();
+        }
+
         public int Somar(int num1, int num2) 
         {
-            return 0;
+            int resultado = num1 + num2;
+            listHistorico.Insert(0, "Resultado: " + resultado);
+
+            return resultado;
         }
 
         public int Subtrair(int num1, int num2)
         {
-            return 0;
+            int resultado = num1 - num2;
+            listHistorico.Insert(0, "Resultado: " + resultado);
+
+            return resultado;
         }
 
         public int Multiplicar(int num1, int num2)
         {
-            return 0;
+            int resultado = num1 * num2;
+            listHistorico.Insert(0, "Resultado: " + resultado);
+
+            return resultado;
         }
 
         public int Dividir(int num1, int num2)
         {
-            return 0;
+            int resultado = num1 / num2;
+            listHistorico.Insert(0, "Resultado: " + resultado);
+
+            return resultado;
         }
 
         public List<string> Historico() 
         {
-            return new List<string>();
+            listHistorico.RemoveRange(3, listHistorico.Count - 3);
+            return listHistorico;
         }
     }
 }
